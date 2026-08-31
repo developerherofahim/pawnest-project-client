a11ba5baffa4e9ca8414c34176bdffc7aedf5333
developerherofahim <developerherofahim@gmail.com>
2026-09-01 02:44:03 +0600

    increase jwt security duration

--- src/lib/auth.js
+++ src/lib/auth.js
@@ -30,7 +30,7 @@ export const auth = betterAuth({
         cookieCache: {
             enabled: true,
             strategy:'jwt',
-            maxAge: 7*24*60*60
+            maxAge: 60*24*60*60
         }
     },
     plugins: [
